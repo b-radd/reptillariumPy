0955d3107ccd3d485f6a77d6d17d3c29eacf5027
bradleyd90@icloud.com <SepGBG8ezS9szb{"%'c|>
2023-06-27 20:32:14 +1000

    change

--- app.py
+++ app.py
@@ -75,7 +75,17 @@ def index():
                 dict(count=1, label="1y", step="year", stepmode="backward"),
                 dict(step="all")
             ])
-        )
+        ),
+        tickformatstops = [
+            dict(dtickrange=[None, 1000], value="%H:%M:%S.%L ms"),
+            dict(dtickrange=[1000, 60000], value="%H:%M:%S s"),
+            dict(dtickrange=[60000, 3600000], value="%H:%M m"),
+            dict(dtickrange=[3600000, 86400000], value="%H:%M h"),
+            dict(dtickrange=[86400000, 604800000], value="%e. %b d"),
+            dict(dtickrange=[604800000, "M1"], value="%e. %b w"),
+            dict(dtickrange=["M1", "M12"], value="%b '%y M"),
+            dict(dtickrange=["M12", None], value="%Y Y")
+        ]
     )
     # fig.update_yaxes(title_text='Temperature (ºC)', range=[10., 40.])
     fig.update_yaxes(title_text='Temperature (ºC)',)
