4505308ed5ec935237aeb55d55d5496fdc5471ce
bradleyd90@icloud.com <SepGBG8ezS9szb{"%'c|>
2023-06-27 19:38:58 +1000

    change

--- test_db.py
+++ test_db.py
@@ -3,7 +3,11 @@ import sqlite3
 
 # Read sqlite query results into a pandas DataFrame
 conn = sqlite3.connect("./db/sensorData.sqlite")
-df = pd.read_sql_query("SELECT * FROM measurements WHERE measureType='temperature'", conn)
+df = pd.read_sql_query(
+    """
+    SELECT * FROM measurements WHERE measureType='temperature' AND vivarium_id=1
+    """,
+    conn)
 
 # Verify that result of SQL query is stored in the dataframe
 print(df.to_string())
