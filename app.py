19c21ef639cad31d65591f0aad49a2dc14d87083
bradleyd90@icloud.com <SepGBG8ezS9szb{"%'c|>
2024-01-06 09:03:27 +1100

    change

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
 import sqlite3
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 import json
 import plotly
 import plotly.express as px
+import plotly.graph_objects as go
 import serial
 from waitress import serve
 
@@ -63,43 +64,119 @@ def index():
 
     df = pd.read_sql_query(
         """
-        SELECT * FROM measurements WHERE measureType='temperature' AND vivarium_id=1
+        SELECT * FROM measurements WHERE measureType='temperature' AND vivarium_id=1 AND timestamp>= datetime('now','-2 day')
         """
         , conn)
 
     conn.close()
 
-    fig = px.line(df, x='timestamp', y='value', color='sensor_position', markers=True)
-    fig.update_xaxes(title_text='Time')
-    fig.update_xaxes(
-        rangeslider_visible=False,
-        rangeselector=dict(
-            buttons=list([
-                dict(count=3, label="3h", step="hour", stepmode="backward"),
-                dict(count=6, label="6h", step="hour", stepmode="backward"),
-                dict(count=12, label="12h", step="hour", stepmode="backward"),
-                dict(count=24, label="1d", step="hour", stepmode="todate"),
-                dict(count=7, label="1w", step="day", stepmode="backward"),
-                dict(count=14, label="2w", step="day", stepmode="backward"),
-                # dict(count=3, label="1q", step="month", stepmode="backward"),
-                # dict(count=6, label="1/2y", step="month", stepmode="backward"),
-                # dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all")
-            ])
-        ),
-        tickformatstops=[
-            # dict(dtickrange=[None, 1000], value="%H:%M:%S.%L ms"),
-            dict(dtickrange=[1000, 60000], value="%I:%M:%S %p"),
-            dict(dtickrange=[60000, 3600000], value="%I:%M %p"),
-            dict(dtickrange=[3600000, 86400000], value="%I %p"),
-            dict(dtickrange=[86400000, 604800000], value="%e. %b"),
-            dict(dtickrange=[604800000, "M1"], value="%e. %b"),
-            dict(dtickrange=["M1", "M12"], value="%b '%y"),
-            dict(dtickrange=["M12", None], value="%Y")
-        ]
-    )
+    fig = go.Figure()
+
+    for group, data in df.groupby('sensor_position'):
+        fig.add_trace(go.Line(
+            x=data['timestamp'],
+            y=data['value'],
+            name=group,
+            yaxis='y'
+        )
+        )
+
+    conn = sqlite3.connect(sqlite_db)
+
+    df = pd.read_sql_query(
+        """
+        SELECT * FROM measurements WHERE measureType='humidity' AND vivarium_id=1 AND timestamp>= datetime('now','-2 day')
+        """
+        , conn)
+
+    conn.close()
+
+    for group, data in df.groupby('sensor_position'):
+        fig.add_trace(go.Line(
+            x=data['timestamp'],
+            y=data['value'],
+            name=group,
+            yaxis='y2'
+        )
+        )
+
+    # fig = px.line(df, x='timestamp', y='value', color='sensor_position', markers=True)
+    # fig.update_xaxes(title_text='Time')
+    # fig.update_xaxes(
+    #     rangeslider_visible=False,
+    #     rangeselector=dict(
+    #         buttons=list([
+    #             dict(count=3, label="3h", step="hour", stepmode="backward"),
+    #             dict(count=6, label="6h", step="hour", stepmode="backward"),
+    #             dict(count=12, label="12h", step="hour", stepmode="backward"),
+    #             dict(count=24, label="1d", step="hour", stepmode="todate"),
+    #             dict(count=7, label="1w", step="day", stepmode="backward"),
+    #             dict(count=14, label="2w", step="day", stepmode="backward"),
+    #             # dict(count=3, label="1q", step="month", stepmode="backward"),
+    #             # dict(count=6, label="1/2y", step="month", stepmode="backward"),
+    #             # dict(count=1, label="1y", step="year", stepmode="backward"),
+    #             dict(step="all")
+    #         ])
+    #     ),
+    #     tickformatstops=[
+    #         # dict(dtickrange=[None, 1000], value="%H:%M:%S.%L ms"),
+    #         dict(dtickrange=[1000, 60000], value="%I:%M:%S %p"),
+    #         dict(dtickrange=[60000, 3600000], value="%I:%M %p"),
+    #         dict(dtickrange=[3600000, 86400000], value="%I %p"),
+    #         dict(dtickrange=[86400000, 604800000], value="%e. %b"),
+    #         dict(dtickrange=[604800000, "M1"], value="%e. %b"),
+    #         dict(dtickrange=["M1", "M12"], value="%b '%y"),
+    #         dict(dtickrange=["M12", None], value="%Y")
+    #     ]
+    # )
     # fig.update_yaxes(title_text='Temperature (ºC)', range=[10., 40.])
-    fig.update_yaxes(title_text='Temperature (ºC)',)
+    # fig.update_yaxes(title_text='Temperature (ºC)',)
+    fig.update_layout(
+        xaxis=dict(
+            title_text='Time',
+            rangeslider_visible=False,
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=3, label="3h", step="hour", stepmode="backward"),
+                    dict(count=6, label="6h", step="hour", stepmode="backward"),
+                    dict(count=12, label="12h", step="hour", stepmode="backward"),
+                    dict(count=24, label="1d", step="hour", stepmode="todate"),
+                    dict(count=7, label="1w", step="day", stepmode="backward"),
+                    dict(count=14, label="2w", step="day", stepmode="backward"),
+                    # dict(count=3, label="1q", step="month", stepmode="backward"),
+                    # dict(count=6, label="1/2y", step="month", stepmode="backward"),
+                    # dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(step="all")
+                ])
+            ),
+            tickformatstops=[
+                # dict(dtickrange=[None, 1000], value="%H:%M:%S.%L ms"),
+                dict(dtickrange=[1000, 60000], value="%I:%M:%S %p"),
+                dict(dtickrange=[60000, 3600000], value="%I:%M %p"),
+                dict(dtickrange=[3600000, 86400000], value="%I %p"),
+                dict(dtickrange=[86400000, 604800000], value="%e. %b"),
+                dict(dtickrange=[604800000, "M1"], value="%e. %b"),
+                dict(dtickrange=["M1", "M12"], value="%b '%y"),
+                dict(dtickrange=["M12", None], value="%Y")
+            ]
+        ),
+
+        yaxis=dict(
+            title_text='Temperature (ºC)',
+            titlefont=dict(color="#1f77b4"),
+            tickfont=dict(color="#1f77b4"),
+            showgrid=False,
+        ),
+
+        yaxis2=dict(
+            title_text="yaxis2 title",
+            overlaying="y",
+            side="right",
+            position=1.,
+            showgrid=False,
+        ),
+    )
+
 
     graphJSON_1 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
@@ -107,7 +184,7 @@ def index():
 
     df = pd.read_sql_query(
         """
-        SELECT * FROM measurements WHERE measureType='temperature' AND vivarium_id=2
+        SELECT * FROM measurements WHERE measureType='temperature' AND vivarium_id=2 AND timestamp>= datetime('now','-2 day')
         """
         , conn)
 
--- test_db.py
+++ test_db.py
@@ -1,15 +1,28 @@
 import pandas as pd
 import sqlite3
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+
+now = datetime.now()
+backdate = now - timedelta(days=3)
 
 # Read sqlite query results into a pandas DataFrame
 conn = sqlite3.connect("./db/sensorData.sqlite")
 df = pd.read_sql_query(
     """
-    SELECT * FROM measurements WHERE measureType='temperature' AND vivarium_id=1
+    SELECT * FROM measurements WHERE measureType='humidity' AND timestamp>= datetime('now','-2 day')
     """,
-    conn)
+    con=conn)
 
 # Verify that result of SQL query is stored in the dataframe
-print(df.to_string())
+# print(df.to_string())
 
 conn.close()
+
+print(df.to_string())
+
+# for group, data in df.groupby('vivarium_id'):
+#     print(group)
+#     data[['timestamp', 'value']].plot()
+#
+# plt.show()
