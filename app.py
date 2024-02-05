384d87f8ffb061bd7af4036ed463f5f6db35a9f7
bradleyd90@icloud.com <SepGBG8ezS9szb{"%'c|>
2024-02-06 08:52:31 +1100

    change

--- app.py
+++ app.py
@@ -223,10 +223,58 @@ def index():
 
     graphJSON_2 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
+    conn = sqlite3.connect(sqlite_db)
+
+    df = pd.read_sql_query(
+        """
+        SELECT * FROM measurements WHERE measureType='humidity' AND vivarium_id=2
+        """
+        , conn)
+
+    conn.close()
+
+    fig = px.line(df, x='timestamp', y='value', color='sensor_position', markers=True)
+    fig.update_xaxes(title_text='Time')
+    fig.update_xaxes(
+        rangeslider_visible=False,
+        rangeselector=dict(
+            buttons=list([
+                dict(count=3, label="3h", step="hour", stepmode="backward"),
+                dict(count=6, label="6h", step="hour", stepmode="backward"),
+                dict(count=12, label="12h", step="hour", stepmode="backward"),
+                dict(count=24, label="1d", step="hour", stepmode="backward"),
+                # dict(count=7 * 24, label="1w", step="hour", stepmode="backward"),
+                # dict(count=14, label="2w", step="day", stepmode="backward"),
+                # dict(count=3, label="1q", step="month", stepmode="backward"),
+                # dict(count=6, label="1/2y", step="month", stepmode="backward"),
+                # dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        tickformatstops=[
+            # dict(dtickrange=[None, 1000], value="%H:%M:%S.%L ms"),
+            dict(dtickrange=[1000, 60000], value="%I:%M:%S %p"),
+            dict(dtickrange=[60000, 3600000], value="%I:%M %p"),
+            dict(dtickrange=[3600000, 86400000], value="%I %p"),
+            dict(dtickrange=[86400000, 604800000], value="%e. %b"),
+            dict(dtickrange=[604800000, "M1"], value="%e. %b"),
+            # dict(dtickrange=["M1", "M12"], value="%b '%y"),
+            # dict(dtickrange=["M12", None], value="%Y")
+        ]
+    )
+
+    graphJSON_3 = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
     # return df_vi1.to_html()
     # return render_template('index.html', **templateData)
     # return render_template('index.html', **templateData, graphJSON=graphJSON)
-    return render_template('index.html', **templateData, graphJSON_1=graphJSON_1, graphJSON_2=graphJSON_2)
+    return render_template(
+        'index.html',
+        **templateData,
+        graphJSON_1=graphJSON_1,
+        graphJSON_2=graphJSON_2,
+        graphJSON_3=graphJSON_3,
+    )
 
 
 if __name__ == '__main__':
